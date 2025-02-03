c5f999f7ea86843742a8903101f96696da64105c
dario flavio <d.scipioni@dstech.it>
2025-02-03 11:08:07 +0100

    EsercitazioneUpdate

--- Application/ApplicativoCorsoJava/src/main/java/it/dst/formazione/esercitazioni/esercitazione2/DarioFlavioScipioni/untitled/src/main/java/org/example/Main.java
+++ Application/ApplicativoCorsoJava/src/main/java/it/dst/formazione/esercitazioni/esercitazione2/DarioFlavioScipioni/untitled/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.ambientazione.Corridoio;
 import org.example.ambientazione.Scenario;
 import org.example.eventi.Eventi;
 import org.example.giocatore.Persona;
@@ -17,17 +18,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Persona personaggio = new Persona();
         System.out.println("Benvenuto in Cosmic Impact!\nCome ti chiami.\nInserisci nome: ");
-        personaggio.setNome(scanner.nextLine());
         LocalDateTime inizio = LocalDateTime.now();
+        personaggio.setNome(scanner.nextLine());
         System.out.println("Benvenuto comandante: " + personaggio.getNome());
-        System.out.println("Da dove provieni?\n>");
+        System.out.println("Da che pianeta provieni?");
         personaggio.setProvenienza(scanner.nextLine());
+        System.out.println("NOME-> " + personaggio.getNome());
+        System.out.println("PIANETA DI PROVENIENZA-> " +personaggio.getProvenienza()+"\n");
         System.out.println("La tua navicella dopo aver impattato un asteroide ha attivato il sistema di sicurezza che prevede l'ibernazione dell'equipaggio.\nAvete vagato per 3 anni nello spazio, ma purtroppo sei l'unico sopravvissuto.\nPer tua fortuna sei approdato in questa stazione spaziale abbadonata.\nBenvenuto a EPSILON 9.\nTrova il modo di riparare la tua nave e tornare sul Pianeta " + personaggio.getProvenienza());
 
-        System.out.println("Scegli il tuo ruolo");
+        System.out.println("\nScegli il tuo ruolo");
         Ruolo umano = new Ruolo("Umano ", "L'umano è un personaggio equilibrato tra attacco e difesa, consigliato per chi si approccia al gioco per la prima volta. ", Arrays.asList("Empatico", "Equilibrato"));
         Ruolo alieno = new Ruolo("Alieno ", "L'alieno è un personaggio che punta molto sul suo equipaggiamento innovativo, ha pochi punti vita, ma un attacco alto. ", Arrays.asList("Innovativo", "PotenteArsenale"));
-        Ruolo robot = new Ruolo("Robot", "Il robot è un personaggio molto resistente, con un attacco basso, ma con molti punti vita. ", Arrays.asList("ScudoDifensivo", "Intoccabile"));
+        Ruolo robot = new Ruolo("Robot ", "Il robot è un personaggio molto resistente, con un attacco basso, ma con molti punti vita. ", Arrays.asList("ScudoDifensivo", "Intoccabile"));
         System.out.println(">1 " + umano.getNome() + "Spiegazione: " + umano.getDescrizione() + "Abilità: " + umano.getAbilita());
         System.out.println(">2 " + alieno.getNome() + "Spiegazione: " + alieno.getDescrizione() + "Abilità: " + alieno.getAbilita());
         System.out.println(">3 " + robot.getNome() + "Spiegazione: " + robot.getDescrizione() + "Abilità: " + robot.getAbilita());
@@ -43,23 +46,16 @@ public class Main {
             System.out.println("Hai scelto: " + robot.getNome().toUpperCase());
 
         } else {
-            System.out.println("Non corrisponde a nessuna classe");
-        }
-
-        /*switch (sceltaPersonaggio) {
-            case 1:
+            System.out.println("Non corrisponde a nessuna classe\nSeleziona 1, 2 oppure 3");
+            int sceltaPersonaggio2 = scanner.nextInt();
+            if (sceltaPersonaggio2 == 1) {
                 System.out.println("Hai scelto: " + umano.getNome().toUpperCase());
-                break;
-
-            case 2:
+            } else if (sceltaPersonaggio2 == 2) {
                 System.out.println("Hai scelto: " + alieno.getNome().toUpperCase());
-                break;
-
-            case 3:
+            } else if (sceltaPersonaggio2 == 3) {
                 System.out.println("Hai scelto: " + robot.getNome().toUpperCase());
-
-            default:
-                System.out.println("Non corrisponde a nessuna classe");*/
+            }
+        }
 
         Scenario percorso1 = new Scenario("Corridoio buio, sembra pericoloso");
         Scenario percorso2 = new Scenario("Sala macchine");
@@ -73,15 +69,23 @@ public class Main {
         int sceltaPercorso = scanner.nextInt();
 
 
-        if (sceltaPercorso == 1) {
-            System.out.println("Ti trovi nel " + percorso1.getPercorso());
-        } else if (sceltaPercorso == 2) {
-            System.out.println("Ti trovi nella " + percorso2.getPercorso());
-        } else if (sceltaPercorso == 3) {
-            System.out.println("Ti trovi nei " + percorso3.getPercorso());
-        } else {
-            System.out.println("Scelta non valida");
-        }
+    if (sceltaPercorso == 1) {
+        System.out.println("Ti trovi nel " + percorso1.getPercorso());
         Eventi.generaAvvenimentoRandom();
+    } else if (sceltaPercorso == 2) {
+        System.out.println("Ti trovi nella " + percorso2.getPercorso());
+        Eventi.generaAvvenimentoRandom();
+
+    } else if (sceltaPercorso == 3) {
+        System.out.println("Ti trovi nei " + percorso3.getPercorso());
+        Eventi.generaAvvenimentoRandom();
+
+    } else {
+        System.out.println("Scelta non valida");
+    }
+
+        LocalDateTime fine = LocalDateTime.now();
+        long durata = java.time.Duration.between(inizio,fine).getSeconds();
+        System.out.println("Tempo di gioco: " +durata+" secondi.");
     }
 }
