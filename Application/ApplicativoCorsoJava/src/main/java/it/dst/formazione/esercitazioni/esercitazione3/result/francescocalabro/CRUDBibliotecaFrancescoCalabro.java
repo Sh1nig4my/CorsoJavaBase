51acb818e741193a95e82c1756c4fc7c8e69b094
FrancescoDst <f.calabro@dstech.it>
2025-02-06 14:19:42 +0100

    Esercizio revisionato

--- Application/ApplicativoCorsoJava/src/main/java/it/dst/formazione/esercitazioni/esercitazione3/result/francescocalabro/CRUDBibliotecaFrancescoCalabro.java
+++ Application/ApplicativoCorsoJava/src/main/java/it/dst/formazione/esercitazioni/esercitazione3/result/francescocalabro/CRUDBibliotecaFrancescoCalabro.java
@@ -1,26 +1,18 @@
 package it.dst.formazione.esercitazioni.esercitazione3.result.francescocalabro;
-
 import it.dst.formazione.esercitazioni.esercitazione3.BibliotecaInterface;
 import it.dst.formazione.esercitazioni.esercitazione3.Libro;
-
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-//import static it.dst.formazione.lezioni.lezione13.JDBCConnection.getConnection;
-//import static it.dst.formazione.tools.InputOuyputConst.libri;
+import static it.dst.formazione.tools.InputOutputConst.*;
 
 public class CRUDBibliotecaFrancescoCalabro implements BibliotecaInterface {
-
-    // TODO: potrebbe essere una soluzione la cattura di una ecceione specifica per SQL e propagare l'errore? Parlo per tutti i metodi
-
 
     private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
     private static final String USER = "root";
     private static final String PASSWORD = "Kenpachi92#";
 
-    public static Connection getConnection() {
+    public static Connection getConnection() throws SQLException{
         try {
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
@@ -31,13 +23,6 @@ public class CRUDBibliotecaFrancescoCalabro implements BibliotecaInterface {
 
     @Override
     public String createTableLibro() {
-        // TODO: la query la puoi prendere dall'interfaccia inputOutputConst
-        String query = "CREATE TABLE IF NOT EXISTS libri ("
-                + "id INT AUTO_INCREMENT PRIMARY KEY, "
-                + "titolo VARCHAR(100) NOT NULL, "
-                + "autore VARCHAR(100) NOT NULL, "
-                + "anno_pubblicazione INT NOT NULL, "
-                + "disponibile BOOLEAN DEFAULT TRUE)";
 
         try (Connection conn = getConnection();
              Statement stmt = conn.createStatement()) { // INFO: secondo te come mai questo warning?
@@ -47,27 +32,14 @@ public class CRUDBibliotecaFrancescoCalabro implements BibliotecaInterface {
 
         } catch (SQLException e) {
             System.err.println("Errore nella creazione della tabella: " + e.getMessage());
+            throw new RuntimeException();
         }
-        return ""; // TODO: la stringa la puoi prendere dall'interfaccia inputOutputConst
+        return resultString;
     }
 
     @Override
     public String testInserimento() {
         String query = "INSERT INTO libri (titolo, autore, anno_pubblicazione, disponibile) VALUES (?, ?, ?, ?)";
-        // TODO: la list la puoi prendere dall'interfaccia inputOutputConst
-        List<Libro> libri = Arrays.asList(
-                new Libro("I Promessi Sposi", "Alessandro Manzoni", 1827, true),
-                new Libro("1984", "George Orwell", 1949, true),
-                new Libro("Il Piccolo Principe", "Antoine de Saint-Exupéry", 1943, true),
-                new Libro("Harry Potter e la Pietra Filosofale", "J.K. Rowling", 1997, true),
-                new Libro("Il Signore degli Anelli", "J.R.R. Tolkien", 1954, false),
-                new Libro("Il Nome della Rosa", "Umberto Eco", 1980, true),
-                new Libro("Orgoglio e Pregiudizio", "Jane Austen", 1813, true),
-                new Libro("Delitto e Castigo", "Fëdor Dostoevskij", 1866, true),
-                new Libro("Il Processo", "Franz Kafka", 1925, true),
-                new Libro("Moby Dick", "Herman Melville", 1851, true)
-        );
-
         try (Connection conn = getConnection();
              PreparedStatement pstmt = conn.prepareStatement(query)) { // INFO: secondo te come mai questo warning?
 
@@ -85,8 +57,9 @@ public class CRUDBibliotecaFrancescoCalabro implements BibliotecaInterface {
             System.out.println("Libro inserito con successo.");
         } catch (SQLException e) {
             System.err.println("Errore durante l'inserimento: " + e.getMessage());
+            throw new RuntimeException();
         }
-        return ""; // TODO: la stringa la puoi prendere dall'interfaccia inputOutputConst
+        return resultString;
     }
 
     @Override
@@ -107,6 +80,7 @@ public class CRUDBibliotecaFrancescoCalabro implements BibliotecaInterface {
             }
         } catch (SQLException e) {
             System.err.println("Errore durante il recupero dei dati: " + e.getMessage());
+            throw new RuntimeException();
         }
         return libri;
     }
@@ -131,6 +105,7 @@ public class CRUDBibliotecaFrancescoCalabro implements BibliotecaInterface {
             }
         } catch (SQLException e) {
             System.err.println("Errore durante il recupero dati: " + e.getMessage());
+            throw new RuntimeException();
         }
         return libriFiltrati;
     }
@@ -150,8 +125,9 @@ public class CRUDBibliotecaFrancescoCalabro implements BibliotecaInterface {
 
         } catch (SQLException e) {
             System.err.println("Errore nell'aggiornamento: " + e.getMessage());
+            throw new RuntimeException();
         }
-        return ""; // TODO: la stringa la puoi prendere dall'interfaccia inputOutputConst
+        return resultString;
     }
 
     @Override
@@ -167,8 +143,9 @@ public class CRUDBibliotecaFrancescoCalabro implements BibliotecaInterface {
 
         } catch (SQLException e) {
             System.err.println("Errore nell'eliminazione: " + e.getMessage());
+            throw new RuntimeException();
         }
-        return ""; // TODO: la stringa la puoi prendere dall'interfaccia inputOutputConst
+        return resultString;
     }
 
 }
